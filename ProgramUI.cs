74c230f6f8ecf85fcf1f3dde17e25562325a5cb1
Alexandria Hardin <aphardin101@gmail.com>
2021-06-28 08:16:10 -0500

    rough draft

---
+++ ProgramUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Komodo_Insurance
+{
+    class ProgramUI
+    {
+        protected readonly DeveloperRepo _streamingRepo//fix this name = new DeveloperRepos();
+
+        //This is the method that runs our User Interface
+        public void Run()
+        {
+            //Putting in seed data to have some starting data
+            SeedContentList();
+
+            DisplayMenu();
+    }
+}
